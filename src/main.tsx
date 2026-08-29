0852f66340f7b34d6b2a11b8f2a53bc68540992f
haydenkua <haydenkua2011@gmail.com>
2026-08-29 22:52:18 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import App from './App';
+import './index.css'; // 👈 This line tells Vite to treat it as a style file
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
