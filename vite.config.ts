e510d9d76d7bfc333dbf56bec47afc114a1c2d4d
haydenkua <haydenkua2011@gmail.com>
2026-08-29 22:07:17 +0800

    Add import for fileURLToPath in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { fileURLToPath } from 'url'; // 👈 Add this import
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 
-// 👈 Create an ESM compatible __dirname replacement
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
